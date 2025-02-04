46cad795e14e8ab06e6bfdce328194c3ec97ac36
Leonardo Alchieri <leonardo.alchieri@usi.ch>
2025-02-04 14:11:49 +0100

    Minor fixes and readme

--- lionplot/plot.py
+++ lionplot/plot.py
@@ -5,6 +5,10 @@ from matplotlib.lines import Line2D
 from matplotlib import colors as mcolors
 
 plt_markers: list[str] = list(Line2D.markers.keys())
+# remove ',' from the list of markers
+plt_markers.remove(',')
+# remove 'o' from the list of markers
+plt_markers.remove('o')
 
 
 def lionplot(
@@ -23,21 +27,19 @@ def lionplot(
     
     # TODO: handle arbitrary length of hue_values
     colors: list[tuple[float, float, float]] = [mcolors.to_rgb(color) for color in plt.rcParams['axes.prop_cycle'].by_key()['color']]
-    
-    lower_bounds = np.asarray([abs(y_val - yerr_val) for y_val, yerr_val in zip(y, yerr)])
-    upper_bounds = np.asarray([abs(yerr_val - y_val) for y_val, yerr_val in zip(y, yerr)])
+
 
     unique_category_vals = np.unique(hue_values)
     for i, category_val in enumerate(unique_category_vals):
         subset_x = x[hue_values == category_val]
         subset_y = y[hue_values == category_val]
-        subset_lower_bounds = lower_bounds[hue_values == category_val]
-        subset_upper_bounds = upper_bounds[hue_values == category_val]
+        subset_yerr = yerr[hue_values == category_val]
+        
         ax.errorbar(
             x=subset_x,
             y=subset_y,
-            yerr=(subset_lower_bounds, subset_upper_bounds),
-            label=category_val if len(unique_category_vals) == 0 else None,
+            yerr=subset_yerr,
+            label=category_val if len(unique_category_vals) > 0 else None,
             elinewidth=elinewidth,
             linestyle="none",
             markersize=markersize,
--- tests/__init__.py
+++ tests/__init__.py
@@ -14,10 +14,11 @@ num_samples = 6
 x = np.asarray([category for i in range(num_samples//len(categories)) for category in categories])
 
 # Generate the y variable from a Gaussian distribution with mean 10
-y = np.random.normal(loc=10, scale=1, size=num_samples)
+y = np.random.normal(loc=10, scale=1, size=num_samples//2)
+y = np.append(y, np.random.normal(loc=15, scale=1, size=num_samples//2))
 
 # Generate the yerr variable from a Gaussian distribution with mean 1
-yerr = np.random.normal(loc=1, scale=0.1, size=num_samples)
+yerr = np.random.normal(loc=1, scale=0.001, size=num_samples)
 
 # Create a group value
 group = ["A" for i in range(num_samples//2)]
@@ -25,7 +26,7 @@ group = group + ["B" for i in range(num_samples//2)]
 group = np.asarray(group)
 
 golden_ratio = (5**0.5 - 1) / 2
-figsize = 3.2
+figsize = 5
     
 fig, ax = plt.subplots(1, 1, figsize=(figsize / golden_ratio, figsize))
 lionplot(
@@ -35,4 +36,6 @@ lionplot(
     yerr=yerr,
     hue_values=group,
 )
-plt.show()
+ax.legend()
+plt.savefig("./imgs/example.pdf")
+plt.close()
